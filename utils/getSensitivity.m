d7e609a032dc5154a4af7ec83cabfaca6863d513
Giovanni Bordiga <gio.61192@gmail.com>
2015-08-29 14:32:28 +0200

    Add sensitivity analysis function

---
+++ utils/getSensitivity.m
@@ -0,0 +1,26 @@
+function dC = getSensitivity(nelx, nely, x, CoPen, Ke, U)
+% Return the sensitivity of the Lagrangian with respect to densities
+% 'nelx' and 'nely' are the number of element along the two dimensions.
+% 'x' is a nely-by-nelx matrix representing the density field on the plate.
+% 'CoPen' is the penalization coefficient used in the SIMP model.
+% 'Ke' is the stiffness matrix of an element (assuming that is the same for
+% every element).
+% 'U' is the global dofs vector returned by FEM function.
+
+dC = zeros(nely, nelx);
+for elx = 1:nelx
+    for ely = 1:nely
+        eDOFindex = [];     % global dofs index of the current element, needed to access the element's dofs from 'U'
+        nodesnum = [(elx-1)*(nely+1) + ely + 1
+                    (elx)*(nely+1) + ely + 1
+                    (elx)*(nely+1) + ely
+                    (elx-1)*(nely+1) + ely];    % global nodes numbers of the current element
+        for i = 1:n
+            eDOFindex = cat(2, DOFindex, (nodesnum(i)-1)*ndof+1:nodesnum(i)*ndof);
+        end
+        Ue = U(eDOFindex);      % dofs of the current element
+        dC(ely, elx) = - CoPen * x(ely, elx)^(CoPen-1) * Ue'*Ke*Ue;
+    end
+end
+end
+
