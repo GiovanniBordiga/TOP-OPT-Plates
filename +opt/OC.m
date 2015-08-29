5fbb6eb5b0683d84bdc0cc24e46814770dc2c6b7
Giovanni Bordiga <gio.61192@gmail.com>
2015-08-29 17:58:53 +0200

    Change the convergence tolerance to a more reasonable value

--- +opt/OC.m
+++ +opt/OC.m
@@ -6,7 +6,7 @@ l1 = 0; l2 = 1e20;  % limits to the volume Lagrange multiplier
 move = 0.2;         % limit to the change of 'x'
 FS = 0.5;           % stabilization factor
 % find the volume multiplier using a bisection method
-while (l2-l1 > 1e-10) % TODO test other tollernaces, like 1e-5
+while (l2-l1 > 1e-4)
     lmid = 0.5*(l2+l1);
     xnew = max(0.001, max(x-move, min(1., min(x+move, x.*(max(0.0001,-dC)./lmid).^FS))));
     if sum(sum(xnew)) - FrVol*nelx*nely > 0
