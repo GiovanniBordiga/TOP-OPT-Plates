3a4f2c7db1148fad016d2d8c07e4f4574fa4e9dc
Giovanni Bordiga <gio.61192@gmail.com>
2015-08-29 19:58:42 +0200

    Implement Newton method to find the volume multiplier insted of the bisection method

--- +opt/OC.m
+++ +opt/OC.m
@@ -11,13 +11,23 @@ l1 = 0; l2 = 1e9;  % limits to the volume Lagrange multiplier
 move = 0.2;         % limit to the change of 'x'
 FS = 0.5;           % stabilization factor
 % find the volume multiplier using a bisection method
-while (l2-l1 > 1e-4)
-    lmid = 0.5*(l2+l1);
-    xnew = max(0.001, max(x-move, min(1., min(x+move, x.*(max(0.0001,-dC)./lmid).^FS))));
-    if sum(sum(xnew)) - FrVol*nelx*nely > 0
-        l1 = lmid;
-    else
-        l2 = lmid;
-    end
+% while (l2-l1 > 1e-4)
+%     lmid = 0.5*(l2+l1);
+%     xnew = max(0.001, max(x-move, min(1., min(x+move, x.*(max(0.0001,-dC)./lmid).^FS))));
+%     if sum(sum(xnew)) - FrVol*nelx*nely > 0
+%         l1 = lmid;
+%     else
+%         l2 = lmid;
+%     end
+% end
+
+% find the volume multiplier using the Newton method
+l = 0.5*(l2+l1);
+xnew = max(0.001, max(x-move, min(1., min(x+move, x.*(max(0.0001,-dC)./l).^FS))));
+totvol = sum(sum(xnew));
+while abs(totvol - FrVol*nelx*nely) > 1e-1
+    totvol = sum(sum(xnew));
+    l = l - (totvol - FrVol*nelx*nely)/(totvol*(-FS/l));
+    xnew = max(0.001, max(x-move, min(1., min(x+move, x.*(max(0.0001,-dC)./l).^FS))));
 end
 end
