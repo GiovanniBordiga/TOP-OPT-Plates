f57797127fe42054efe559cbde827e7d5e9dd470
Giovanni Bordiga <gio.61192@gmail.com>
2015-09-01 20:40:26 +0200

    Complete 'FE' implementation with useful methods

--- +FEM/FE.m
+++ +FEM/FE.m
@@ -1,16 +1,30 @@
 classdef FE
-    %FE Summary of this class goes here
-    %   Detailed explanation goes here
+    % Class representing a finite element.
     
     properties
-        type
-        dims
+        type            % string representing the element type
+        dims            % struct which defines the element's dimensions: width, height, thickness
+        nodes           % number of nodes
+        ndof            % number of dof per node
     end
     
     methods
         function obj = FE(type, dims)
             obj.type = type;
             obj.dims = dims;
+            switch type
+                % Kirchhoff
+                case 'ACM'
+                    obj.nodes = 4;
+                    obj.ndof = 3;
+                case 'BMF'
+                    obj.nodes = 4;
+                    obj.ndof = 4;
+                % Mindlin
+                case 'MB4'
+                    obj.nodes = 4;
+                    obj.ndof = 3;
+            end
         end
         function type = getType(obj)
             type = obj.type;
@@ -26,6 +40,12 @@ classdef FE
         end
         function thickness = getThickness(obj)
             thickness = obj.dims.thickness;
+        end
+        function nodes = getNodes(obj)
+            nodes = obj.nodes;
+        end
+        function ndof = getNDof(obj)
+            ndof = obj.ndof;
         end
     end
     
