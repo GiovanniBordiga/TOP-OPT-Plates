1bbac5864a5e5b33eae8565eee8560d3ed1e5514
Giovanni Bordiga <gio.61192@gmail.com>
2015-09-01 17:27:00 +0200

    Add plot functions (convergence and density plots)

---
+++ +plot/plotConvergence.m
@@ -0,0 +1,21 @@
+function plotConvergence(iter, values, obj)
+% Plot convergence plots.
+% 'iter' is a vector containing the number of iterations.
+% 'values' is a vector containing the values to plot.
+% 'obj' is a caracter representing the type of convergence plot.
+
+switch obj
+    case 'c'
+        % plot convergence in terms of compliance
+        figure(1);
+        plot(iter, values);
+        title('Convergence of the objective function: Compliance');
+        xlabel('Iterations'); ylabel('Compliance');
+    case 'x'
+        % plot convergence in terms of density change
+        figure(2);
+        plot(iter, values);
+        title('Convergence of the design variable: Density');
+        xlabel('Iterations'); ylabel('Density change');
+end
+end
---
+++ +plot/plotDesign.m
@@ -0,0 +1,13 @@
+function plotDesign(x)
+% Plot the density field.
+% 'x' is a matrix representing the density field on the plate.
+
+figure(3);                      % focus the corresponding figure
+colormap(flipud(gray));         % set a gray-scale colormap
+imagesc(x);                     % display 'x'
+grid;                           % show the grid
+colorbar;                       % display colorbar
+axis equal; axis tight;         % set axis to match plate dimensions
+title('Density field');         % set title
+xlabel('x'); ylabel('y');       % set axis labels
+end
