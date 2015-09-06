add120db8e873f80c11989cfb3dae2e621739385
Giovanni Bordiga <gio.61192@gmail.com>
2015-09-06 16:28:14 +0200

    Add the Sensitivity Analysis function for eigenfrequencies

---
+++ +opt/getFSensitivity.m
@@ -0,0 +1,31 @@
+function dF = getFSensitivity(nelx, nely, element, x, PenK, PenM, Ke, Me, eigenF, eigenM)
+% Return the sensitivity of the lowest eigenfrequency with respect to densities.
+% 'nelx' and 'nely' are the number of elements along the two dimensions.
+% 'element' is a FE object.
+% 'x' is a nely-by-nelx matrix representing the density field on the plate.
+% 'PenK' and 'PenM' are the penalization coefficients used in the SIMP model.
+% 'Ke' and 'Me' are, respectively, the stiffness matrix and the mass matrix
+% of an element (assuming that is the same for every element).
+% 'eigenF' is the vector of the eigenvalues.
+% 'eigenM' is the matrix of the eigenvectors (stored as columns).
+
+n = element.getNodes();     % nodes
+ndof = element.getNDof();   % dofs per node
+dF = zeros(nely, nelx);
+lowF = eigenF(end);         % get the lowest eigenfrequency
+lowM = eigenM(:, end);      % get the corresponding eigenmode
+for elx = 1:nelx
+    for ely = 1:nely
+        eDOFindex = [];     % global dofs index of the current element, needed to access the element's dofs from 'lowM'
+        nodesnum = [(elx-1)*(nely+1) + ely + 1
+                    (elx)*(nely+1) + ely + 1
+                    (elx)*(nely+1) + ely
+                    (elx-1)*(nely+1) + ely];    % global nodes numbers of the current element
+        for i = 1:n
+            eDOFindex = cat(2, eDOFindex, (nodesnum(i)-1)*ndof+1:nodesnum(i)*ndof);
+        end
+        lowMe = lowM(eDOFindex);      % dofs of the current element
+        dF(ely, elx) = lowMe'*(PenK*x(ely, elx)^(PenK-1)*Ke - lowF * PenM*x(ely, elx)^(PenM-1)*Me)*lowMe;
+    end
+end
+end
