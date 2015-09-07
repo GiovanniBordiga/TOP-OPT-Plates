74c30727393bbb83c549b50cd620f69924bba3f9
Giovanni Bordiga <gio.61192@gmail.com>
2015-09-07 15:28:50 +0200

    Add explanatory comment to 'PG' function

--- +opt/PG.m
+++ +opt/PG.m
@@ -1,6 +1,13 @@
 function xnew = PG(nelx, nely, x, FrVol, dF)
-%PG Summary of this function goes here
-%   Detailed explanation goes here
+% Implement a Projected Gradient method.
+% 'nelx' and 'nely' are the number of elements along the two dimensions.
+% 'x' is a nely-by-nelx matrix representing the density field on the plate.
+% 'FrVol' is the desired volume fraction at the optimum condition.
+% 'dF' is a nely-by-nelx matrix returned by the sensitivity analysis.
+
+% WARNING: this code is written assuming unit element's area.
+%          to minimize compliance pass dF.
+%          to maximize eigenfrequency pass -dF.
 
 stepsize = 1;
 l = sum(sum(dF))/(nelx*nely);
